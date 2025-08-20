25de25f60fc53dace1ab2983fea1821f91f4d97d
zombachu <zombachu@gmail.com>
2025-08-19 23:11:09 -0700

    Correctly mark root for commands with require() as the root

--- stick-core/src/main/kotlin/structure/Commands.kt
+++ stick-core/src/main/kotlin/structure/Commands.kt
@@ -1,11 +1,11 @@
 package com.zombachu.stick.structure
 
+import com.zombachu.stick.Command
 import com.zombachu.stick.impl.Requirement
-import com.zombachu.stick.impl.SenderScope
 import com.zombachu.stick.impl.StructureScope
 import com.zombachu.stick.lowercase
 
-fun <S> SenderScope<S>.command(
+fun <S> Command<S>.command(
     name: String,
     aliases: Set<String> = setOf(),
     requirement: Requirement<S> = Requirement { true },
@@ -30,7 +30,7 @@ fun <S> StructureScope<S>.command(
         name.lowercase(),
         aliases.lowercase(),
         description,
-        parent = this,
+        parent = if (this.parent == null) null else this,
         requirement,
     )
 }
