bf0a1b8d923849056b2ac0ca24088b6f8a2268f5
zombachu <zombachu@gmail.com>
2025-08-27 01:35:32 -0700

    Add Detekt
    TBD whether I'll use it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     id("org.jetbrains.dokka") version "2.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "io.gitlab.arturbosch.detekt").also {
+        detekt {
+            buildUponDefaultConfig = true
+            allRules = false
+        }
+
+        tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+            exclude("**/test/**")
+        }
+    }
+
 }
 
 tasks.dokkaHtmlMultiModule {
