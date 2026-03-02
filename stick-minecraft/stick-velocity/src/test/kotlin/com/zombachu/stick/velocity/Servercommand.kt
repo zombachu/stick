ada01ddd6fb2ef9864914d0ac9a191c8c79202fe
zombachu <zombachu@gmail.com>
2026-03-01 23:05:52 -0800

    Update velocity test command

--- stick-minecraft/stick-velocity/src/test/kotlin/com/zombachu/stick/velocity/Servercommand.kt
+++ stick-minecraft/stick-velocity/src/test/kotlin/com/zombachu/stick/velocity/Servercommand.kt
@@ -2,6 +2,7 @@ package com.zombachu.stick.velocity
 
 import com.velocitypowered.api.command.CommandSource
 import com.zombachu.stick.structure.command
+import com.zombachu.stick.structure.id
 import com.zombachu.stick.structure.intParameter
 import com.zombachu.stick.structure.invoke
 import com.zombachu.stick.structure.nullableValueFlag
@@ -9,15 +10,19 @@ import com.zombachu.stick.structure.optionallyNullable
 import com.zombachu.stick.structure.stringParameter
 
 class ServerCommand : VelocityCommand<CommandSource> {
+
+    val nullableIntId = id<Int?>("null")
+    val nonNullIntId = id<Int>("notnull")
+
     override val structure =
         command("test")(
             nullableValueFlag(
-                name = "hey",
+                nullableIntId,
                 parameter = intParameter("someInt"),
             ),
             optionallyNullable(stringParameter("someString"))
         ) { flag: Int?, optional: String? ->
-
+            val result: Int? = this.get(nullableIntId)
         }
 
 }
