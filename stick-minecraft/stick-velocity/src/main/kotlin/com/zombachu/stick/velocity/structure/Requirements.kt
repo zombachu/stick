7b6d213df70d5cd31088be3d7a7bb784d675bdf1
zombachu <zombachu@gmail.com>
2026-03-28 20:51:52 -0700

    Allow arbitrary error types for velocity's permission()

--- stick-minecraft/stick-velocity/src/main/kotlin/com/zombachu/stick/velocity/structure/Requirements.kt
+++ stick-minecraft/stick-velocity/src/main/kotlin/com/zombachu/stick/velocity/structure/Requirements.kt
@@ -1,14 +1,16 @@
 package com.zombachu.stick.velocity.structure
 
 import com.velocitypowered.api.command.CommandSource
+import com.zombachu.stick.CommandResult
+import com.zombachu.stick.SenderValidationResult
 import com.zombachu.stick.impl.BuilderScope
 import com.zombachu.stick.impl.Requirement
 import com.zombachu.stick.structure.requirement
 import com.zombachu.stick.velocity.VelocityEnvironment
 
-// TODO: Permission message
 fun <E : VelocityEnvironment, S : CommandSource> BuilderScope<E, S>.permission(
-    permission: String
-): Requirement<E, S> = requirement {
+    permission: String,
+    failureResult: () -> CommandResult.Failure<Unit> = SenderValidationResult::failPermission,
+): Requirement<E, S> = requirement(failureResult) {
     it.sender.hasPermission(permission)
 }
