96e0cafedbbaf32648b8819cd03ec88eddb24422
zombachu <zombachu@gmail.com>
2026-04-07 00:15:43 -0700

    Bump version to 0.1.0

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.zombachu.stick"
-version = "0.0.7"
+version = "0.1.0"
 
 kotlin {
     // Use a specific Java version to make it easier to work in different environments.
